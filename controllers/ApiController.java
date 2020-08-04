424c47b47b04e1f4667820fd84aa766be06dbeaf
sexyuck <sexyuck@gmail.com>
2020-08-04 21:25:58 +0900

    [NP0001] 기능개발

---
+++ controllers/ApiController.java
@@ -0,0 +1,5 @@
+public class ApiController {
+  public void main(String[] args) {
+    System.put.println("java");
+  }
+}
---
+++ repository/VueRePository.java
@@ -0,0 +1,5 @@
+public class VueRepository{
+  public void main(String[] args) {
+    System.out.println("hi");
+  }
+}
